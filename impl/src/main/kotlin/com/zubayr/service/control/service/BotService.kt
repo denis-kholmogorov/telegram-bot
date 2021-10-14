48e23afdefb584058e8f66284625c5609d322f4a
paul <pwasilevroot@gmail.com>
2021-10-14 12:31:57 +0300

    log added

--- impl/src/main/kotlin/com/zubayr/service/control/service/BotService.kt
+++ impl/src/main/kotlin/com/zubayr/service/control/service/BotService.kt
@@ -1,11 +1,9 @@
 package com.zubayr.service.control.service
 
-import com.zubayr.service.control.constants.CONFIG_MAPS
-import com.zubayr.service.control.constants.ONE_MAP
 import io.fabric8.kubernetes.client.KubernetesClient
-import org.springframework.beans.factory.annotation.Autowired
+import org.apache.logging.log4j.kotlin.Logging
+import org.jvnet.hk2.annotations.Service
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.stereotype.Service
 import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Update
@@ -14,9 +12,9 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKe
 
 @Service
 class BotService(
-    val kubernetesClient: KubernetesClient,
-    val configMapService: ConfigMapService
-) : TelegramLongPollingCommandBot() {
+        val kubernetesClient: KubernetesClient,
+        val configMapService: ConfigMapService
+) : TelegramLongPollingCommandBot(), Logging {
 
 
     @Value("\${telegram.bot.token}")
@@ -26,8 +24,8 @@ class BotService(
     private val botName: String? = null
 
     val frases = mutableListOf(
-        "Матока", "Бананонина!", "Белло!", "Пупай!", "Пара ту", "По тае то пара ту",
-        "Ти амо пупай!", "Лук ат ту", "Папой, теремика папой? а папой"
+            "Матока", "Бананонина!", "Белло!", "Пупай!", "Пара ту", "По тае то пара ту",
+            "Ти амо пупай!", "Лук ат ту", "Папой, теремика папой? а папой"
     )
 
     override fun getBotToken() = token
@@ -37,6 +35,8 @@ class BotService(
     override fun processNonCommandUpdate(update: Update) {
         if (update.hasMessage() && update.message.hasText()) {
             val messageInput = update.message
+            logger.info { "processing nonCommandUpdate $messageInput" }
+
             val messageOutput = SendMessage()
             when {
                 messageInput.text.contains("/start") -> {
@@ -45,24 +45,20 @@ class BotService(
                     messageOutput.text = "Белло! ${messageInput.from.firstName}"
                     //execute(SendSticker(messageInput.chatId.toString(), InputFile(File(stiker.random()))))
                     execute(messageOutput.apply { replyMarkup = createButton() })
+                    logger.info { "answer ${messageOutput.text}" }
                 }
-                messageInput.text.contains(CONFIG_MAPS) -> {
-                    messageOutput.chatId = messageInput.chatId.toString()
-                    messageOutput.text = "Матока! Вот все Мапы"
-                    messageOutput.replyMarkup = configMapService.getAllConfigMaps()
-                    //execute(SendSticker(messageInput.chatId.toString(), InputFile(File(stiker.random()))))
-                    execute(messageOutput)
-                }
-
                 messageInput.text.startsWith("/bye") -> {
                     //  execute(SendSticker(messageInput.chatId.toString(), InputFile(File("impl/src/main/resources/buy.webp"))))
                     messageOutput.chatId = messageInput.chatId.toString()
                     messageOutput.text = "Hasta la vista! Baby ${messageInput.from.firstName}"
+                    logger.info { "answer ${messageOutput.text}" }
+
                 }
                 else -> {
                     messageOutput.chatId = messageInput.chatId.toString()
                     messageOutput.text = frases.random()
                     execute(messageOutput)
+                    logger.info { "answer ${messageOutput.text}" }
                 }
             }
         } else if (update.hasCallbackQuery()) {
@@ -71,12 +67,6 @@ class BotService(
                     execute(SendMessage().apply {
                         chatId = update.getId()
                         text = frases.random()
-                    })
-                }
-                update.callbackQuery.data.startsWith(ONE_MAP) -> {
-                    execute(SendMessage().apply {
-                        chatId = update.getId()
-                        text = configMapService.getConfigMapByName(update.callbackQuery.data)
                     })
                 }
                 update.callbackQuery.data.startsWith("/bye") -> {
@@ -100,8 +90,8 @@ class BotService(
             callbackData = "/bye"
         }
         val keyboardButtonsRow1 = mutableListOf(
-            buttonSpeak,
-            buttonBye
+                buttonSpeak,
+                buttonBye
         )
         inlineKeyboardMarkup.keyboard = mutableListOf(keyboardButtonsRow1)
         return inlineKeyboardMarkup
